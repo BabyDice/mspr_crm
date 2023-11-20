f2560b9cd2c77c2f016775e7ed8e80970db87de9
BabyDice <secure.contain.protect10888@gmail.com>
2023-11-20 15:57:46 +0100

    Manjaro ajout de quelques fichier entity

---
+++ src/main/java/com/msprcrm/msprcrm/entity/Produit.java
@@ -0,0 +1,112 @@
+package com.msprcrm.msprcrm.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+@Entity
+public class Produit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
+    private Long ident;
+
+    private String nom;
+
+    private String type;
+
+    private double prix;
+
+    private String adresse;
+
+    private String code_postal;
+
+    private String ville;
+
+    private String superficie;
+
+    private String nb_piece;
+
+    private String description;
+
+
+
+    public Long getIdent() {
+        return ident;
+    }
+
+    public void setIdent(Long ident) {
+        this.ident = ident;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getCode_postal() {
+        return code_postal;
+    }
+
+    public void setCode_postal(String code_postal) {
+        this.code_postal = code_postal;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getSuperficie() {
+        return superficie;
+    }
+
+    public void setSuperficie(String superficie) {
+        this.superficie = superficie;
+    }
+
+    public String getNb_piece() {
+        return nb_piece;
+    }
+
+    public void setNb_piece(String nb_piece) {
+        this.nb_piece = nb_piece;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
