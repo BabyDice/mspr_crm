a2218f3ffa9c1ec5213b506b7b635d0824e3587d
Mehdi Essendi <mehdi.essendi@gmail.com>
2023-11-22 16:34:51 +0100

    ADD STATS

---
+++ src/main/java/com/msprcrm/msprcrm/entity/ProduitStatistique.java
@@ -0,0 +1,9 @@
+package com.msprcrm.msprcrm.entity;// ProduitStatistique.java
+
+public class ProduitStatistique {
+    private Long productId;
+    private String nomProduit;
+    private long totalVentes;
+
+    // Constructeur, getters, setters
+}
